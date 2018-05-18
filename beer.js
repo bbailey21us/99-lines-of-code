4040db721e5a12b9001bda1a61882e36a522a363
bbailey21us <brandonbailey21@gmail.com>
2018-05-18 15:08:29 -0400

    added a conditional statement

--- beer.js
+++ beer.js
@@ -7,7 +7,10 @@ for (i=0;i<friends.length;i++){
     for(r=99;r>0;r--){
        var number = r;
        var next =r-1;
-        
+       if(next===0){
+           next="no more"
+       } 
        console.log(number+" "+"lines of code in the file, "+number+" "+"lines of code;"+" "+friends[i]+" strikes one out, clears it all out "+next+" lines of code in the file" )
+       
     }
 }
