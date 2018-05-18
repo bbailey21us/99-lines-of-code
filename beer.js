4ca607968fbbac1c30228951a426ab9fc4ecc717
bbailey21us <brandonbailey21@gmail.com>
2018-05-18 15:04:07 -0400

    added a variable in beer.js

---
+++ beer.js
@@ -0,0 +1,13 @@
+var friends =["bill", "ted", "joe","josh", "nick"]
+
+
+
+
+for (i=0;i<friends.length;i++){
+    for(r=99;r>0;r--){
+       var number = r;
+       var next =r-1;
+        
+       console.log(number+" "+"lines of code in the file, "+number+" "+"lines of code;"+" "+friends[i]+" strikes one out, clears it all out "+next+" lines of code in the file" )
+    }
+}
