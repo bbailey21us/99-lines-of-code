9dca36b04c27314b8761f839d9402d5065bd2892
bbailey21us <brandonbailey21@gmail.com>
2018-05-18 15:23:21 -0400

    added a little javascript

--- beer.js
+++ beer.js
@@ -5,12 +5,14 @@ var friends =["bill", "ted", "joe","josh", "nick"]
 
 for (i=0;i<friends.length;i++){
     for(r=99;r>0;r--){
-       var number = r;
        var next =r-1;
-       if(next===0){
+       if(r===99){
+           console.log(friends[i]);
+       }
+       else if(next===0){
            next="no more"
        } 
-       console.log(number+" "+"lines of code in the file, "+number+" "+"lines of code;"+" "+friends[i]+" strikes one out, clears it all out "+next+" lines of code in the file" )
+       console.log(r+" "+"lines of code in the file, "+r+" "+"lines of code;"+" "+friends[i]+" strikes one out, clears it all out "+next+" lines of code in the file" )
        
     }
 }
